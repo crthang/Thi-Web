dc7fe5d9035563e566ac42f8ec66c29ae7165bd8
Cr Thắng <crthang14@gmail.com>
2017-12-16 11:40:38 +0700

    Update file cau_6.php

---
+++ cau_6.php
@@ -0,0 +1,144 @@
+<html>
+    <head>
+        <title>Trang cau 5</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="" method="POST">
+            <table>
+                <tr>
+                    <th>Trang tìm kiếm</th>
+                </tr>
+                <tr>
+                    <td>Tìm kiếm</td>
+                    <td><input type="text" name="timkiem"></td>
+                </tr>
+                <tr>
+                    <td>Tìm theo:</td>
+                    <td>
+                     <input type="radio" name="ndtk" value="theoten" checked="true" >Theo Tên
+                     <input type="radio" name="ndtk" value="theobiethieu">Theo biệt hiệu
+                     <input type="radio" name="ndtk" value="theodienthoai">Theo điện thoại
+                    </td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Tìm kiếm" name="btntim"></td>
+                    <td><input type="reset" value="Tìm lại"></td>
+                </tr>
+            </table>
+        </form>
+        </body>
+</html>
+
+<?php
+    if(isset($_POST['btntim']))
+    {
+        $tim=$_POST['timkiem'];
+        $timtheo=$_POST['ndtk'];
+        if(($timtheo=="theoten")) 
+        {
+            echo "<h1>TÌM KIẾM THEO TÊN</h1>";
+            include("cau_2.php");
+            mysqli_set_charset($con, 'UTF8');
+            $sql="SELECT * FROM lopcu WHERE hoVaTen LIKE '%$tim%' ORDER BY id ASC ";
+            $query=mysqli_query($con,$sql);
+            $number=mysqli_num_rows($query);
+            if($number>0)
+            {   
+            $i=0;
+            echo "<table border='1' ><tr><th>STT</th><th>Họ tên</th>";
+            echo "<th>Mã thành viên</th><th>Biệt hiệu</th><th>Mật khẩu</th><th>Email</th><th>Điện thoại</th>";
+            echo "<th>Địa chỉ</th><th>Nơi làm việc</th><th>Chức vụ</th>";
+            echo "<th>Xóa</th>";
+            while($row=mysqli_fetch_array($query))
+            {
+                $i++;
+                $link_xoa="../hoc_php/xoa_thanh_vien.php?&id=".$row["id"];
+                echo "<tr><td>$i</td>";
+                echo "<td>".$row["hoVaTen"]."</td><td>".$row["maThanhVien"]."</td>";
+                echo "<td>".$row["bietHieu"]."</td><td>".$row["matKhau"]."</td>";
+                echo "<td>".$row["email"]."</td><td>".$row["dienThoai"]."</td>";
+                echo "<td>".$row["diaChi"]."</td><td>".$row["noiLamViec"]."</td>";
+                echo "<td>".$row["chucVu"]."</td>";
+                echo "<td><a href=".$link_xoa.">Xóa</a></td>";
+            }
+            echo "</table>";
+            }
+            else
+            {
+            echo "Không có bản ghi nào thỏa mãn";
+            }
+        
+        }
+            if(($timtheo=="theobiethieu")) 
+        {
+            echo "<h1>TÌM KIẾM THEO BIỆT HIỆU</h1>";
+            include("cau_2.php");
+            mysqli_set_charset($con, 'UTF8');
+            $sql="SELECT * FROM lopcu WHERE bietHieu LIKE '%$tim%' ORDER BY id ASC ";
+            $query=mysqli_query($con,$sql);
+            $number=mysqli_num_rows($query);
+            if($number>0)
+            {   
+            $i=0;
+            echo "<table border='1' ><tr><th>STT</th><th>Họ tên</th>";
+            echo "<th>Mã thành viên</th><th>Biệt hiệu</th><th>Mật khẩu</th><th>Email</th><th>Điện thoại</th>";
+            echo "<th>Địa chỉ</th><th>Nơi làm việc</th><th>Chức vụ</th>";
+            echo "<th>Xóa</th>";
+            while($row=mysqli_fetch_array($query))
+            {
+                $i++;
+                $link_xoa="../hoc_php/xoa_thanh_vien.php?&id=".$row["id"];
+                echo "<tr><td>$i</td>";
+                echo "<td>".$row["hoVaTen"]."</td><td>".$row["maThanhVien"]."</td>";
+                echo "<td>".$row["bietHieu"]."</td><td>".$row["matKhau"]."</td>";
+                echo "<td>".$row["email"]."</td><td>".$row["dienThoai"]."</td>";
+                echo "<td>".$row["diaChi"]."</td><td>".$row["noiLamViec"]."</td>";
+                echo "<td>".$row["chucVu"]."</td>";
+                echo "<td><a href=".$link_xoa.">Xóa</a></td>";
+            }
+            echo "</table>";
+            }
+            else
+            {
+            echo "Không có bản ghi nào thỏa mãn";
+            }
+        
+        }
+            if(($timtheo=="theodienthoai")) 
+        {
+            echo "<h1>TÌM KIẾM THEO ĐIỆN THOẠI</h1>";
+            include("cau_2.php");
+            mysqli_set_charset($con, 'UTF8');
+            $sql="SELECT * FROM lopcu WHERE dienThoai LIKE '%$tim%' ORDER BY id ASC ";
+            $query=mysqli_query($con,$sql);
+            $number=mysqli_num_rows($query);
+            if($number>0)
+            {   
+            $i=0;
+            echo "<table border='1' ><tr><th>STT</th><th>Họ tên</th>";
+            echo "<th>Mã thành viên</th><th>Biệt hiệu</th><th>Mật khẩu</th><th>Email</th><th>Điện thoại</th>";
+            echo "<th>Địa chỉ</th><th>Nơi làm việc</th><th>Chức vụ</th>";
+            echo "<th>Xóa</th>";
+            while($row=mysqli_fetch_array($query))
+            {
+                $i++;
+                $link_xoa="../hoc_php/xoa_thanh_vien.php?&id=".$row["id"];
+                echo "<tr><td>$i</td>";
+                echo "<td>".$row["hoVaTen"]."</td> <td>".$row["maThanhVien"]."</td>";
+                echo "<td>".$row["bietHieu"]."</td><td>".$row["matKhau"]."</td>";
+                echo "<td>".$row["email"]."</td><td>".$row["dienThoai"]."</td>";
+                echo "<td>".$row["diaChi"]."</td><td>".$row["noiLamViec"]."</td>";
+                echo "<td>".$row["chucVu"]."</td>";
+                echo "<td><a href=".$link_xoa.">Xóa</a></td>";
+            }
+            echo "</table>";
+            }
+            else
+            {
+            echo "Không có bản ghi nào thỏa mãn";
+            }
+        
+        }
+    }
+?>
---
+++ xoa_thanh_vien.php
@@ -0,0 +1,110 @@
+<?php 
+ include('cau_2.php');
+    $id=$_GET['id'];
+    $tv="select * from lopcu where id='$id' ";
+    $tv_1=mysqli_query($con,$tv);
+    $tv_2=mysqli_fetch_array($tv_1);
+    $maThanhVien=$tv_2['maThanhVien'];
+    $hoVaTen=$tv_2['hoVaTen'];
+    $bietHieu=$tv_2['bietHieu'];
+    $matKhau=$tv_2['matKhau'];
+    $email=$tv_2['email'];
+    $dienThoai=$tv_2['dienThoai'];
+    $diaChi=$tv_2['diaChi'];
+    $noiLamViec=$tv_2['noiLamViec'];
+    $chucVu=$tv_2['chucVu'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Xóa thành viên</title>
+      <SCRIPT LANGUAGE="JavaScript">
+      function confirmAction() {
+        return confirm("Bạn có muốn xóa Tài Khoản <?php echo $hoVaTen; ?> ")
+      }
+ 
+      </SCRIPT>
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data" onSubmit="return confirmAction()" > 
+    <table width="990px" >
+        <tr>
+            <td colspan="2" ><b style="color:blue;font-size:20px" >Xóa Thông Tin: <?php echo $hoVaTen; ?> </b><br><br> </td>
+            
+        </tr>
+          <tr>
+            <td width="150px" >Mã Thành viên : </td>
+            <td width="840px">
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" name="maThanhVien" value="<?php echo $maThanhVien; ?>" >
+            </td>
+        </tr>
+          <tr>
+            <td width="150px" >Họ và Tên : </td>
+            <td width="840px">
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" name="hoVaTen" value="<?php echo $hoVaTen; ?>" >
+            </td>
+        </tr>
+        <tr>
+            <td width="150px" >Biệt hiệu : </td>
+            <td width="840px">
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" name="bietHieu" value="<?php echo $bietHieu; ?>" >
+            </td>
+        </tr>        
+        <tr>
+            <td>Mật khẩu : </td>
+            <td>
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" type="password" name="matKhau" value="<?php echo $matKhau; ?>" >
+            </td>
+        </tr>
+           <tr>
+            <td width="150px" >Email : </td>
+            <td width="840px">
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" type="email" name="email" value="<?php echo $email; ?>" >
+            </td>
+        </tr>
+        <tr>
+            <td width="150px" >Điện thoại : </td>
+            <td width="840px">
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" name="dienThoai" value="<?php echo $dienThoai; ?>" >
+            </td>
+        </tr>
+        <tr>
+            <td width="150px" >Địa chỉ : </td>
+            <td width="840px">
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" name="diaChi" value="<?php echo $diaChi; ?>" >
+            </td>
+        </tr>
+        <tr>
+            <td width="150px" >Chức vụ : </td>
+            <td width="840px">
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" name="chucVu" value="<?php echo $chucVu; ?>" >
+            </td>
+        </tr>
+        
+        <tr>
+            <td valign="top" >Nơi làm việc : </td>
+            <td>
+                <input style="width:400px;margin-top:3px;margin-bottom:3px;" name="noiLamViec" value="<?php echo $noiLamViec; ?>" >
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>
+                <br>
+                <input type="submit" name="bieu_mau_sua" value="Xóa" style="width:200px;height:50px;font-size:24px" >
+            </td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
+<?php 
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   include('cau_2.php');
+     $id=$_GET['id'];
+     $tv=" DELETE FROM lopcu WHERE id =$id; ";
+    mysqli_query($con,$tv);
+    header("location:cau_6.php"); 
+    }
+?>
